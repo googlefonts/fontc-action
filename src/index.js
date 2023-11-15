2f9b090746ce8cb9d0da080f876372f15c4e7f86
Chris Simpkins <chris@sourcefoundry.org>
2023-11-15 14:47:48 -0500

    add debug compile of fontc support with new Action workflow "debug" configuration field

--- src/index.js
+++ src/index.js
@@ -7,26 +7,39 @@ async function run() {
   const fontcGitSha = core.getInput("commit-sha");
   const fontcArgs = core.getInput("args");
   const sourcePath = core.getInput("source-path");
+  const useDebugBuild = core.getInput("debug");
 
   // ==================
   // Install fontc
   // ==================
   try {
+    let buildCommand = "";
+    if (useDebugBuild === "false") {
+      buildCommand = "cargo install --release";
+    } else if (useDebugBuild === "true") {
+      buildCommand = "cargo install --debug";
+    } else {
+      core.setFailed(
+        "fontc-action failed during the attempt to compile fontc. " +
+          "Please define either 'true' or 'false' in the Action workflow debug configuration."
+      );
+    }
+
     if (fontcGitSha !== "none") {
       await exec.exec(
-        `cargo install --rev ${fontcGitSha} --git https://github.com/googlefonts/fontc.git fontc`
+        `${buildCommand} --rev ${fontcGitSha} --git https://github.com/googlefonts/fontc.git fontc`
       );
     } else if (fontcBranch !== "main") {
       await exec.exec(
-        `cargo install --branch ${fontcBranch} --git https://github.com/googlefonts/fontc.git fontc`
+        `${buildCommand} --branch ${fontcBranch} --git https://github.com/googlefonts/fontc.git fontc`
       );
     } else if (fontcVersionTag !== "none") {
       await exec.exec(
-        `cargo install --tag ${fontcVersionTag} --git https://github.com/googlefonts/fontc.git fontc`
+        `${buildCommand} --tag ${fontcVersionTag} --git https://github.com/googlefonts/fontc.git fontc`
       );
     } else {
       await exec.exec(
-        "cargo install --branch main --git https://github.com/googlefonts/fontc.git fontc"
+        `${buildCommand} --branch main --git https://github.com/googlefonts/fontc.git fontc`
       );
     }
   } catch (error) {
